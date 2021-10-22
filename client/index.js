b7c277a8fa1391f7ad2970f8017131657b143f79
alexandre.demeure@univ-grenoble-alpes.fr <alexandre.demeure@univ-grenoble-alpes.fr>
2021-10-22 11:42:45 +0200

    up

---
+++ client/index.js
@@ -0,0 +1,5 @@
+const [bths, bthc, btss, btsc] = document.querySelectorAll("button");
+bths.onclick = async () => console.log(await fetch('http://localhost:8080/noCORS/tete.svg'));
+bthc.onclick = async () => console.log(await fetch('http://localhost:8080/withCORS/tete.svg'));
+btss.onclick = async () => console.log(await fetch('https://localhost:8443/noCORS/tete.svg'));
+btsc.onclick = async () => console.log(await fetch('https://localhost:8443/withCORS/tete.svg'));
--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const https = require('https');
 const fs = require("fs");
 const express = require('express');
 const app = express()
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
 app.use(express.urlencoded({ extended: true }));
+app.use( expressCspHeader({
+    directives: {
+        'default-src': [SELF],
+        'script-src': [SELF, INLINE, 'https://localhost:8443'],
+        'style-src': [SELF, 'mystyles.net', 'https://localhost:8443'],
+        'img-src': ['data:', 'https://localhost:8443'],
+        'worker-src': [NONE],
+        'connect-src': [SELF, 'https://localhost:8443', 'http://localhost:8080']
+        // 'block-all-mixed-content': true
+}} ) );
 app.use(express.static('client'));
 
 
-app.listen(8081, () => console.log("B : Le serveur HTTP écoute sur le port 8081") );
+app.listen(8081, () => console.log("B : Le serveur HTTP  écoute sur le port 8081") );
 
 let priv, cert;
 if (!fs.existsSync("./private.key")) {
@@ -25,5 +36,5 @@ if (!fs.existsSync("./signature.cert")) {
 }
 
 if (!!priv && !!cert) {
-    https.createServer({key: priv, cert}, app).listen(8444, () => console.log("A : Le serveur HTTPS écoute sur le port 8444") )
+    https.createServer({key: priv, cert}, app).listen(8444, () => console.log("B : Le serveur HTTPS écoute sur le port 8444") )
 }
